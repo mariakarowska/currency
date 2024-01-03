e2e6fc7df35be0a39c752955bfbf0fe80424d63d
Maria Karowska <mariakarowska@gmail.com>
2024-01-03 18:29:35 +0100

    deleted incorrect brackets

--- js/script.js
+++ js/script.js
@@ -1,43 +1,42 @@
 {
-const calculateResult = (amount, currency) => {
-  switch (currency) {
-    case "EUR":
-      return 4.46 * amount;
+  const calculateResult = (amount, currency) => {
+    switch (currency) {
+      case "EUR":
+        return 4.46 * amount;
 
-    case "USD":
-      return 4.22 * amount;
+      case "USD":
+        return 4.22 * amount;
 
-    case "CHF":
-      return 4.72 * amount;
+      case "CHF":
+        return 4.72 * amount;
 
-    case "TRY":
-      return 0.15 * amount;
-  }
-};
+      case "TRY":
+        return 0.15 * amount;
+    }
+  };
 
-const onFormSubmit = () => { (event) =>{
-    event.preventDefault();
+  const onFormSubmit = (event) => {
+    
+      event.preventDefault();
 
-const resultElement = document.querySelector(".js-result");
-const amountElement = document.querySelector(".js-amount");
-const currencyElement = document.querySelector(".js-currency");
+      const resultElement = document.querySelector(".js-result");
+      const amountElement = document.querySelector(".js-amount");
+      const currencyElement = document.querySelector(".js-currency");
 
- 
-    const result = calculateResult(amountElement.value, currencyElement.value);
-
-   
+      const result = calculateResult(
+        amountElement.value,
+        currencyElement.value
+      );
 
       resultElement.innerText = result.toFixed(2);
-
-}
-};
-
-const init = () => {
+    };
   
-const formElement = document.querySelector(".js-form");
 
-formElement.addEventListener ("submit", onFormSubmit);
-};
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
 
-init ();
+    formElement.addEventListener("submit", onFormSubmit);
+  };
+
+  init();
 }
